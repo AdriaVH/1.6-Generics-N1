d818d7a0b11dd49d9696163d83d4b28aa963b4f6
adriaVH <adriavargashernandez@gmail.com>
2025-03-25 11:20:42 +0100

    correction

--- src/ex2/classes/GenericMethods.java
+++ src/ex2/classes/GenericMethods.java
@@ -2,7 +2,7 @@ package ex2.classes;
 
 public class GenericMethods {
 
-    public <T, K, V> void print (T thing1, K thing2, V thing3){
+    public <T, K, V> void print(T thing1, K thing2, V thing3) {
         System.out.println(thing1);
         System.out.println(thing2);
         System.out.println(thing3);
